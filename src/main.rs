2603842b47053b2db98d66117f48c074819616a2
kitswd1103 <147813977+kitswd1103@users.noreply.github.com>
2023-11-11 21:02:36 +0900

    文章を単語で分ける機能作成

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,62 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let text = match env::args().skip(1).next() {
+        Some(text) => text,
+        _ => "".to_string()
+    };
+
+    for word in text_split(text) {
+        println!("{}", word);
+    }
+}
+
+fn text_split(text: String) -> Vec<String> {
+    let mut ret = Vec::new();
+
+    let mut temp = String::new();
+    let mut in_quote = false;
+
+    for c in text.chars() {
+        if c == '\"' {
+            if !temp.is_empty() {
+                ret.push(temp);
+                temp = "".to_string();
+            }
+            in_quote = !in_quote;
+            continue;
+        }
+        if is_delimiter(c) && !in_quote {
+            if !temp.is_empty() {
+                ret.push(temp);
+                temp = "".to_string();
+            }
+            continue;
+        }
+        temp.push(c);
+    }
+    if !temp.is_empty() {
+        ret.push(temp);
+    }
+    ret
+}
+fn is_delimiter(c: char) -> bool {
+    c == ' ' || c == ',' || c == '.' || c == '\"'
+}
+
+
+#[cfg(test)]
+mod test {
+    use crate::text_split;
+
+    #[test]
+    fn test_text_split() {
+        let text = "aaa bbb, ccc. ddd.  , eee".to_string();
+        let words = text_split(text);
+        assert_eq!(words, ["aaa", "bbb", "ccc", "ddd", "eee"].into_iter().map(str::to_string).collect::<Vec<_>>());
+        let text = "aaa \"bbb, ccc.\" ddd\".  , \"eee".to_string();
+        let words = text_split(text);
+        assert_eq!(words, ["aaa", "bbb, ccc.", "ddd", ".  , ", "eee"].into_iter().map(str::to_string).collect::<Vec<_>>());
+ 
+    }
 }
